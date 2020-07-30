97dde0cbdfd0ab042fc8842600851ff7c23d026f
zachho123 <zachho123@gmail.com>
2020-07-30 15:01:19 -0700

    Add 200.num islands

---
+++ 200.number-of-islands/solution.js
@@ -0,0 +1,12 @@
+/**
+ * Counts the number of distinct islands.
+ * @param {string[][]} grid - Input grid of 1's (land) and 0's (ocean).
+ * @returns {number} The number of islands in the grid.
+ */
+function numIslands(grid) {
+
+}
+
+function runTests() {
+
+}
