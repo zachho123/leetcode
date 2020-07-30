6ef795db39cb489bfbc1d725843f700b5c97e798
zachho123 <zachho123@gmail.com>
2020-07-30 14:37:05 -0700

    Complete 828

---
+++ 828.count-unique-characters-of-all-substrings-of-a-given-string/my-naive-solution.js
@@ -0,0 +1,97 @@
+/**
+ * Finds the sum of unique characters for every substring of the input string.
+ * @param {string} s - The input string.
+ * @returns {number} The sum of unique characters per substring % (10 ^ 9 + 7);
+ */
+function uniqueLetterString(s) {
+    const moduloNumber = Math.pow(10, 7) + 7;
+    let sum = 0;
+
+    if (s.length === 1) {
+        return 1;
+    }
+
+    // Generate every substring, and call countUniqueChars() on them
+    for (let i = 0; i < s.length; i++) {
+        for (let j = i + 1; j <= s.length; j++) {
+            sum += countUniqueChars(s.substring(i, j));
+        }
+    }
+
+    return sum % moduloNumber;
+}
+
+/**
+ * Counts the number of unique characters in the string.
+ * @param {string} s - The input string.
+ * @returns {number} Number of unique characters in the string.
+ */
+function countUniqueChars(s) {
+    let unique = new Set();
+    let notUnique = new Set();
+
+    for (let i = 0; i < s.length; i++) {
+        const currChar = s.charAt(i);
+
+        // If we know the current letter isn't unique, skip it
+        if (notUnique.has(currChar)) {
+            continue;
+        }
+        
+        // If we've already seen the current letter, mark it as not unique
+        // and remove it from the unique set
+        if (unique.has(currChar)) {
+            notUnique.add(currChar);
+            unique.delete(currChar);
+        } else {
+            unique.add(currChar);
+        }
+    }
+
+    return unique.size;
+}
+
+(function runCountUniqueCharsTests() {
+    const tests = [
+        { s: 'A', expected: 1 },
+        { s: 'AA', expected: 0 },
+        { s: 'AB', expected: 2 },
+        { s: 'ABA', expected: 1 }
+    ];
+
+    console.log('RUNNING countUniqueChars() TESTS...');
+
+    tests.forEach( (test) => {
+        let result = countUniqueChars(test.s);
+        let testPassed = result === test.expected ? 'Passed' : 'Failed';
+
+        console.log(`Running test: ${test.s}... - ${testPassed}`);
+        console.log(`\tExpected: ${test.expected}, Got: ${result}`);
+    });
+
+    console.log();
+})();
+
+(function runUniqueLetterStringTests() {
+    const tests = [
+        { s: 'A', expected: 1 },
+        { s: 'AB', expected: 4 },
+        { s: 'ABC', expected: 10 },
+        { s: 'AAAB', expected: 8 },
+        { s: 'ABA', expected: 8 },
+        { s: 'LEETCODE', expected: 92 },
+    ];
+
+    console.log('RUNNING uniqueLetterString() TESTS...');
+
+    tests.forEach( (test) => {
+        console.log(`Running test: ${test.s}...`);
+
+        let result = uniqueLetterString(test.s);
+        let testPassed = result === test.expected ? 'Passed' : 'False';
+
+        console.log(`\t${testPassed} - Expected: ${test.expected}, Got: ${result}`);
+    });
+
+    console.log();
+})();
--- 828.count-unique-characters-of-all-substrings-of-a-given-string/solution.js
+++ 828.count-unique-characters-of-all-substrings-of-a-given-string/solution.js
@@ -1,4 +1,42 @@
 /**
+ * ============
+ * Problem:
+ * ============
+ * Find sum of unique characters for every substring of a given input
+ * string. Unique characters are defined as those which only appear once for 
+ * any given substring.
+ * 
+ * ============
+ * Answer:
+ * ============
+ * The trick here is instead of thinking individually about the # of unique
+ * characters in each substring, let's think about how much each letter position
+ * in the input string contributes to the end sum.
+ * --------------
+ * For Example:
+ * --------------
+ * Say we had string 'AXAXXAX'
+ * Let's focus on the 2nd 'A' that appears (at index 2) and see how we can 
+ * create substrings which have that 'A' as unique.
+ * 
+ * We can take 'A(XA)XXAX' and between '()' is our substring
+ * 
+ * We can see, to have the 2nd 'A' counted as a unique letter:
+ * 1. we need to insert '(' somewhere between the 1st and 2nd 'A'
+ * 2. we need to insert ')' somewhere between the 2nd and 3rd 'A'
+ * 
+ * For restriction 1, we have 2 possibilities
+ * For restriction 2, we have 3 possibilities
+ * 
+ * Therefore, we have 2 * 3 = 6 ways to make a substring where the 2nd 'A' is 
+ * a unique character, ie: the 2nd 'A' (index 2) contributes +6 uniqueness 
+ * count to the final sum.
+ * 
+ * Following this, we can perform this at each position in the input string,
+ * and sum the individual contributions to get the desired final sum.
+ */
+
+/**
  * Finds the sum of unique characters for every substring of the input string.
  * @param {string} s - The input string.
  * @returns {number} The sum of unique characters per substring % (10 ^ 9 + 7);
@@ -7,77 +45,43 @@ function uniqueLetterString(s) {
     const moduloNumber = Math.pow(10, 7) + 7;
     let sum = 0;
 
-    if (s.length === 1) {
-        return 1;
-    }
-
-    // Generate every substring, and call countUniqueChars() on them
-    for (let i = 0; i < s.length; i++) {
-        for (let j = i + 1; j <= s.length; j++) {
-            sum += countUniqueChars(s.substring(i, j));
-        }
-    }
-
-    return sum % moduloNumber;
-}
-
-/**
- * Counts the number of unique characters in the string.
- * @param {string} s - The input string.
- * @returns {number} Number of unique characters in the string.
- */
-function countUniqueChars(s) {
-    let unique = new Set();
-    let notUnique = new Set();
-
+    // Populate a map with keys = chars that appear in input string, and
+    // values = arrays containing the indices where that char appears
+    let charPosMap = new Map();
     for (let i = 0; i < s.length; i++) {
         const currChar = s.charAt(i);
-
-        // If we know the current letter isn't unique, skip it
-        if (notUnique.has(currChar)) {
-            continue;
-        }
-        
-        // If we've already seen the current letter, mark it as not unique
-        // and remove it from the unique set
-        if (unique.has(currChar)) {
-            notUnique.add(currChar);
-            unique.delete(currChar);
+        if (charPosMap.has(currChar)) {
+            charPosMap.get(currChar).push(i);
         } else {
-            unique.add(currChar);
+            charPosMap.set(currChar, [i]);
         }
     }
 
-    return unique.size;
-}
-
-(function runCountUniqueCharsTests() {
-    const tests = [
-        { s: 'A', expected: 1 },
-        { s: 'AA', expected: 0 },
-        { s: 'AB', expected: 2 },
-        { s: 'ABA', expected: 1 }
-    ];
-
-    console.log('RUNNING countUniqueChars() TESTS...');
-
-    tests.forEach( (test) => {
-        let result = countUniqueChars(test.s);
-        let testPassed = result === test.expected ? 'Passed' : 'Failed';
-
-        console.log(`Running test: ${test.s}... - ${testPassed}`);
-        console.log(`\tExpected: ${test.expected}, Got: ${result}`);
+    // For every character, at each position it appears, calc the unique char
+    // contribution.
+    charPosMap.forEach( (charPosArr) => {
+        for (let i = 0; i < charPosArr.length; i++) {
+            // Left is the last occurence of the current char, or -1 if
+            // there is none
+            let left = i > 0 ? charPosArr[i-1] : -1;
+            // Right is the next occurence of the current char, or the input
+            // string length if there is none
+            let right = i < charPosArr.length - 1 ? charPosArr[i+1] : s.length;
+            sum += ((charPosArr[i] - left) * (right - charPosArr[i])) % moduloNumber;
+        }
     });
 
-    console.log();
-})();
+    return sum;
+}
 
-(function runUniqueLetterStringTests() {
+(function runTests() {
     const tests = [
         { s: 'A', expected: 1 },
         { s: 'AB', expected: 4 },
         { s: 'ABC', expected: 10 },
-        { s: 'AAAB', expected: 8 }
+        { s: 'AAAB', expected: 8 },
+        { s: 'ABA', expected: 8 },
+        { s: 'LEETCODE', expected: 92 },
     ];
 
     console.log('RUNNING uniqueLetterString() TESTS...');
