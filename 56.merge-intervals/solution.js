20333460e8047e19f592ced55d086d1a1680d119
zachho123 <zachho123@gmail.com>
2020-08-02 09:04:44 -0700

    Work on 56

---
+++ 56.merge-intervals/solution.js
@@ -0,0 +1,30 @@
+/**
+ * Merges all overlapping intervals.
+ * @param {number[][]} intervals - Collection of intervals.
+ * @returns {number[][]} Collection of merged intervals.
+ */
+function merge(intervals) {
+
+}
+
+(function runTests() {
+    let tests = [
+        {
+            input: [
+                [1,3],
+                [2,6],
+                [8,10],
+                [15,18]
+            ],
+            output: [
+                [1,6],
+                [8,10],
+                [15,18]
+            ]
+        }
+    ];
+
+    tests.forEach(test => {
+
+    });
+})();
