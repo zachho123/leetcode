0f9cfb9a0cadad0bebef3cfdf1d6b797fc31b291
zachho123 <zachho123@gmail.com>
2020-08-02 15:59:34 -0700

    Work on 33

---
+++ 33.search-in-rotated-sorted-array/solution.js
@@ -0,0 +1,27 @@
+/**
+ * Returns the index of the target.
+ * @param {number[]} nums - Array to search.
+ * @param {number} target - Value to search for.
+ */
+function search(nums, target) {
+
+}
+
+(function runTests() {
+    let tests = [
+        {
+            input: {
+                nums: [4, 5, 6, 7, 0, 1, 2],
+                target: 0
+            },
+            output: 4
+        },
+        {
+            input: {
+                nums: [4, 5, 6, 7, 0, 1, 2],
+                target: 3
+            },
+            output: -1
+        }
+    ];
+})();
