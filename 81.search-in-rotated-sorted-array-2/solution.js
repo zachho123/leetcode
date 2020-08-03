d32d72bffa6b58ab0db9a37a258e0e09cfebdf43
zachho123 <zachho123@gmail.com>
2020-08-03 10:59:05 -0700

    Complete 81

--- 81.search-in-rotated-sorted-array-2/solution.js
+++ 81.search-in-rotated-sorted-array-2/solution.js
@@ -10,26 +10,29 @@ function search(nums, target) {
 
     while (start <= end) {
         // Use this form to calc mid to avoid overflow
-        let mid = Math.floor((start + (end - start) / 2);
+        let mid = Math.floor((start + (end - start) / 2));
 
         if (target === nums[mid]) {
             return true;
         }
 
-        if (nums[start] < nums[mid]) { // left part sorted
+        // If we can't tell which side is sorted due to duplicates,
+        // move our edges in
+        if (nums[start] === nums[mid] && nums[mid] === nums[end]) {
+            start++;
+            end--;
+        } else if (nums[start] <= nums[mid]) { // left part sorted
             if (target >= nums[start] && target < nums[mid]) {
                 end = mid - 1;
             } else {
                 start = mid + 1;
             }
-        } else if (nums[start] > nums[mid]) { // right part sorted
+        } else { // right part sorted
             if (target > nums[mid] && target <= nums[end]) {
                 start = mid + 1;
             } else {
                 end = mid - 1;
             }
-        } else {
-            end--;
         }
     }
 
@@ -38,48 +41,13 @@ function search(nums, target) {
 
 (function runTests() {
     let tests = [
-        {
-            input: {
-                nums: [],
-                target: 0
-            },
-            output: false
-        },
-        {
-            input: {
-                nums: [1],
-                target: 1
-            },
-            output: true
-        },
-        {
-            input: {
-                nums: [1, 2],
-                target: 2
-            },
-            output: true
-        },
-        {
-            input: {
-                nums: [2,5,6,0,0,1,2],
-                target: 0
-            },
-            output: true
-        },
-        {
-            input: {
-                nums: [2,5,6,0,0,1,2],
-                target: 3
-            },
-            output: false
-        },
-        {
-            input: {
-                nums: [1,3,1,1,1],
-                target: 3
-            },
-            output: true
-        }
+        { input: { nums: [], target: 0 }, output: false },
+        { input: { nums: [1], target: 1 }, output: true },
+        { input: { nums: [1, 2], target: 2 }, output: true },
+        { input: { nums: [2, 1], target: 1 }, output: true },
+        { input: { nums: [2,5,6,0,0,1,2], target: 0 }, output: true },
+        { input: { nums: [2,5,6,0,0,1,2], target: 3 }, output: false },
+        { input: { nums: [1,3,1,1,1], target: 3 }, output: true }
     ];
 
     tests.forEach(test => {
