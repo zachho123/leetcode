43fb5fa822412da0a4483dbd9b0a54444984b56c
zachho123 <zachho123@gmail.com>
2020-09-14 16:14:20 -0700

    Clean up 937

--- 937.reorder-data-in-log-files/solution.js
+++ 937.reorder-data-in-log-files/solution.js
@@ -4,57 +4,34 @@
  * @return {string[]} - Returns ordered logs.
  */
 function reorderLogFiles(logs) {
-  let ordered = [];
+  let letterLogs = [];
   let digitLogs = [];
 
-  for (let i = 0; i < logs.length; i++) {
-    const curr = logs[i];
-    const idIndex = curr.indexOf(' ');
-    const contentStart = curr.charAt(idIndex + 1);
-
-    if (contentStart >= '0' && contentStart <= '9') {
-      digitLogs.push(curr);
+  for (const log of logs) {
+    if (isDigit(body(log).charAt(0))) {
+      digitLogs.push(log);
     } else {
-      ordered.push(curr);
+      letterLogs.push(log);
     }
   }
 
-  ordered.sort(compareLetterLogs);
-  digitLogs.forEach(log => {
-    ordered.push(log);
-  });
-
-  return ordered;
+  return [...letterLogs.sort(compareLogs), ...digitLogs];
 }
 
-/**
- * Compares two tokenized letter logs
- * @param {string[]} l1 - Tokenized letter log array
- * @param {string[]} l2 - Tokenized letter log array
- * @return {number} - Returns -1 if the first log is lexographically less than 
- * the second log. Returns 1 if the first log is greater, else return 0 if they
- * are equal.
- */
-function compareLetterLogs(l1, l2) {
-  const idIndex1 = l1.indexOf(' ');
-  const idIndex2 = l2.indexOf(' ');
-  // Log 1 identifier and content
-  const id1 = l1.substring(0, idIndex1);
-  const content1 = l1.substring(idIndex1+1);
-  // Log 2 identifier and content
-  const id2 = l2.substring(0, idIndex2);
-  const content2 = l2.substring(idIndex2+1);
-
-  // If the content is equal, compare identifers
-  if (content1 === content2) {
-    if (id1 === id2) {
-      return 0;
-    } else {
-      return id1 < id2 ? -1 : 1;
-    }
-  } else {
-    return content1 < content2 ? -1 : 1;
+function compareLogs(a, b) {
+  const compare = body(a).localeCompare(body(b));
+  if (compare !== 0) {
+    return compare;
   }
+  return a.localeCompare(b);
+}
+
+function isDigit(char) {
+  return char >= '0' && char <= '9';
+}
+
+function body(log) {
+  return log.slice(log.indexOf(' ') + 1);
 }
 
 (function run() {
