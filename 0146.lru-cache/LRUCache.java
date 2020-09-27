794ca22e96ee024db22d391ddeb1da379a56eb2b
zachho123 <zachho123@gmail.com>
2020-09-27 14:48:06 -0700

    Practice 146

--- 0146.lru-cache/LRUCache.java
+++ 0146.lru-cache/LRUCache.java
@@ -1,49 +1,101 @@
-import java.util.LinkedList;
-import java.util.Map;
 import java.util.HashMap;
+// import java.util.LinkedList;
 
-class LRUCache {
-
+public class LRUCache {
+    private HashMap<Integer, Node> cache;
+    // private LinkedList<Node> lru;
     private int capacity;
-    private Map<Integer, Integer> map;
-    private LinkedList<Integer> cache;
-
+    private Node head;
+    private Node tail;
+    
     public LRUCache(int capacity) {
+        this.cache = new HashMap<>();
+        // this.lru = new LinkedList<>();
         this.capacity = capacity;
-        this.map = new HashMap<>();
-        this.cache = new LinkedList<>();
+        this.head = new Node();
+        this.tail = new Node();
+        this.head.next = this.tail;
+        this.tail.prev = this.head;
     }
 
     public int get(int key) {
-        if (map.containsKey(key)) {
-            if (!(cache.getFirst() == key)) {
-                cache.remove((Integer)key);
-                cache.addFirst(key);
-            }
-            return map.get(key);
+        if (this.cache.containsKey(key)) {
+            Node node = cache.get(key);
+            // this.lru.remove(node);
+            // this.lru.addFirst(node);
+            this.remove(node);
+            this.addFirst(node);
+            return node.getValue();
         } else {
             return -1;
         }
     }
 
     public void put(int key, int value) {
-        if (map.containsKey(key)) {
-            cache.remove((Integer)key);
+        if (this.cache.containsKey(key)) {
+            Node node = this.cache.get(key);
+            node.setValue(value);
+            // this.lru.remove(node);
+            // this.lru.addFirst(node);
+            this.remove(node);
+            this.addFirst(node);
         } else {
-            if (cache.size() == capacity) {
-                map.remove(cache.getLast());
-                cache.removeLast();
-            }
-        }
+            Node newNode = new Node(key, value);
 
-        map.put(key, value);
-        cache.addFirst(key);
+            if (this.cache.size() + 1 > this.capacity) {
+                Node toEvict = this.removeLast();
+                this.cache.remove(toEvict.getKey());
+            }
+
+            // this.lru.addFirst(newNode);
+            this.addFirst(newNode);
+            this.cache.put(key, newNode);
+        }
+    }
+
+    private void addFirst(Node newNode) {
+        newNode.next = this.head.next;
+        newNode.prev = this.head;
+        this.head.next.prev = newNode;
+        this.head.next = newNode;
+    }
+
+    private void remove(Node toRemove) {
+        toRemove.prev.next = toRemove.next;
+        toRemove.next.prev = toRemove.prev;
+    }
+
+    private Node removeLast() {
+        Node removed = this.tail.prev;
+        this.remove(removed);
+        return removed;
+    } 
+
+    public class Node {
+        private int key;
+        private int value;
+        public Node prev;
+        public Node next;
+
+        public Node() {}
+    
+        public Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+            this.prev = null;
+            this.next = null;
+        }
+    
+        public int getKey() {
+            return this.key;
+        }
+    
+        public int getValue() {
+            return this.value;
+        }
+    
+        public void setValue(int value) {
+            this.value = value;
+        }
     }
 }
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
- */
