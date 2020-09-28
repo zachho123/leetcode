84490b214d628b9251e7ac1e7497dfa6e0194c23
zachho123 <zachho123@gmail.com>
2020-09-27 18:27:16 -0700

    Practice 21

--- 0021.merge-two-sorted-lists/Solution.java
+++ 0021.merge-two-sorted-lists/Solution.java
@@ -1,42 +1,54 @@
-/*
-https://leetcode.com/problems/merge-two-sorted-lists/
-
-Merge two sorted linked lists and return it as a new list. The new list should
-be made by splicing together the nodes of the first two lists.
-*/
-
 public class Solution {
-
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode head = new ListNode();
         ListNode curr = head;
 
         while (l1 != null || l2 != null) {
-            if (l1 == null) {
-                curr.next = l2;
-                l2 = l2.next;
-            } else if (l2 == null) {
+            while (l1 != null && l2 != null) {
+                if (l1.val < l2.val) {
+                    curr.next = l1;
+                    l1 = l1.next;
+                } else {
+                    curr.next = l2;
+                    l2 = l2.next;
+                }
+                curr = curr.next;
+            }
+
+            while (l1 != null) {
                 curr.next = l1;
+                curr = curr.next;
                 l1 = l1.next;
-            } else if (l1.val <= l2.val) {
-                curr.next = l1;
-                l1 = l1.next;
-            } else {
+            }
+
+            while (l2 != null) {
                 curr.next = l2;
+                curr = curr.next;
                 l2 = l2.next;
             }
-            curr = curr.next;
         }
         curr.next = null;
-        
+
         return head.next;
     }
 
     public class ListNode {
         int val;
         ListNode next;
+
         ListNode() {}
-        ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode, next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+    
     }
 }
