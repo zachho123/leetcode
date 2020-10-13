b2a9317159e181cd78da86f1ea58eedb884b804c
zachho123 <zachho123@gmail.com>
2020-10-13 11:27:22 -0700

    Add 55

---
+++ 0055.jump-game/Solution.java
@@ -0,0 +1,20 @@
+/**
+ * Given array of non-negative integers, you are initially positioned at the
+ * first index of the array.
+ * 
+ * Each element in the array represents your maximum jump length at that
+ * position.
+ * 
+ * Determine if you are able to reach the last index.
+ */
+import java.util.*;
+
+public class Solution {
+    public boolean canJump(int[] nums) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
