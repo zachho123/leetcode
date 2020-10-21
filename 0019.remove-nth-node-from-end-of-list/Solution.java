4447466f09d17d07c96a44c5b6d97a8a3f8a91ae
zachho123 <zachho123@gmail.com>
2020-10-21 15:17:57 -0700

    Work on 19

--- 0019.remove-nth-node-from-end-of-list/Solution.java
+++ 0019.remove-nth-node-from-end-of-list/Solution.java
@@ -1,17 +1,77 @@
 public class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode prev = null;
+        ListNode toRemove = head;
+        ListNode runner = head;
 
+        // position runner n nodes ahead
+        for (int i = 0; i < n; i++) {
+            runner = runner.next;
+        }
+
+        // shift everything 1 node until runner hits null
+        while (runner != null) {
+            prev = toRemove;
+            toRemove = toRemove.next;
+            runner = runner.next;
+        }
+
+        // if new head is null, take next non null node or reutrn null
+        if (prev == null) {
+            if (toRemove.next != null) {
+                return toRemove.next;
+            } else {
+                return null;
+            }
+        } else {
+            prev.next = toRemove.next;
+        }
+
+        return head;
+    }
+
+    private ListNode makeLL(int[] vals) {
+        ListNode head = null;
+
+        for (int i = vals.length - 1; i >= 0; i--) {
+            ListNode node = new ListNode(vals[i], head);
+            head = node;
+        }
+
+        return head;
     }
 
     public static void main(String[] args) {
+        Solution s = new Solution();
 
+        int[][] inputLists = {
+            {1, 2, 3, 4, 5},
+            {1},
+            {1, 2},
+            {1, 2, 3, 4, 5}
+        };
+
+        int[] inputN = {2, 1, 1, 5};
+
+        for (int i = 0; i < inputLists.length; i++) {
+            ListNode n = s.removeNthFromEnd(s.makeLL(inputLists[i]), inputN[i]);
+            if (n == null) {
+                System.out.println("null");
+            } else {
+                while (n != null) {
+                    System.out.print(n.val + " ");
+                    n = n.next;
+                }
+                System.out.print("\n");
+            }
+        }
     }
 }
 
 class ListNode {
     int val;
     ListNode next;
-    
+
     ListNode() {}
 
     ListNode(int val) {
