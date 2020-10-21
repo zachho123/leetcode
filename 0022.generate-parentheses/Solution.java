0d833ed13a2007e624a9cbbd635414d6e637f211
zachho123 <zachho123@gmail.com>
2020-10-21 15:21:08 -0700

    Add 22

---
+++ 0022.generate-parentheses/Solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+/**
+ * Given n pairs of prentheses, write a function to generate all combinations
+ * of well-formed parentheses.
+ */
+
+public class Solution {
+    public List<String> generateParenthesis(int n) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
