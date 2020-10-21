7c884ab5ff8feec2a0da8bd826b407a7641e001a
zachho123 <zachho123@gmail.com>
2020-10-21 14:11:17 -0700

    Add 19

---
+++ 0019.remove-nth-node-from-end-of-list/Solution.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
