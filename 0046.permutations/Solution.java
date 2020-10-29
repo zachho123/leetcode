c54b901b8b9ae8d894ff9fcd08f4ec86f1c6a44b
Zach Ho <zachho123@gmail.com>
2020-10-29 14:15:24 -0700

    Add 46

---
+++ 0046.permutations/Solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    /**
+     * Given a collection of distinct integers, return all possible 
+     * permutations.
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> permute(int[] nums) {
+        
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
